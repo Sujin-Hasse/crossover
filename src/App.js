a9a2b4fb9329c4bb34fee991b2f8feb252cafa97
Sujin-Hasse <2017104858@khu.ac.kr>
2023-06-17 15:19:35 +0900

    싫어

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import GlobalStyle from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { Theme } from "./styles/Theme";
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={Theme}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +21,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
